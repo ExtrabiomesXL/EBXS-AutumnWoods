e88fa747f7e21ef66441dbb9725decaa0d528842
Ammon Lauritzen <allaryin@gmail.com>
2015-05-06 15:18:53 -0500

    Imported client proxy hook from EBXL.

--- src/main/java/extrabiomes/autumn/proxy/ClientProxy.java
+++ src/main/java/extrabiomes/autumn/proxy/ClientProxy.java
@@ -1,5 +1,17 @@
 package extrabiomes.autumn.proxy;
 
-public class ClientProxy extends CommonProxy {
+import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
+import cpw.mods.fml.client.registry.RenderingRegistry;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
+@SideOnly(Side.CLIENT)
+public class ClientProxy extends CommonProxy {
+	@Override
+    public int registerBlockHandler(ISimpleBlockRenderingHandler handler)
+    {
+        final int renderId = RenderingRegistry.getNextAvailableRenderId();
+        RenderingRegistry.registerBlockHandler(renderId, handler);
+        return renderId;
+    }
 }
--- src/main/java/extrabiomes/autumn/proxy/CommonProxy.java
+++ src/main/java/extrabiomes/autumn/proxy/CommonProxy.java
@@ -1,10 +1,15 @@
 package extrabiomes.autumn.proxy;
 
+import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
+
 public class CommonProxy {
 
 	public void registerRenderInformation() {
 		// TODO Auto-generated method stub
-		
+	}
+
+	public int registerBlockHandler(ISimpleBlockRenderingHandler handler) {
+		return 0;
 	}
 
 }
