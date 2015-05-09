24ae6212529f2b942e57983a6b5122c5d202c7be
Ammon Lauritzen <allaryin@gmail.com>
2015-05-09 17:02:59 -0500

    Registered worldgen for flowers.

--- src/main/java/extrabiomes/autumn/handlers/BlockHandler.java
+++ src/main/java/extrabiomes/autumn/handlers/BlockHandler.java
@@ -5,7 +5,9 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import extrabiomes.autumn.blocks.BlockExtraFlower;
 import extrabiomes.autumn.stuff.BiomeCollection;
 import extrabiomes.autumn.stuff.BlockCollection;
+import extrabiomes.autumn.worldgen.DecorationGenerator;
 import extrabiomes.lib.ExtraItem;
+import extrabiomes.lib.WorldGenDecoration;
 import extrabiomes.lib.settings.BlockSettings;
 
 public abstract class BlockHandler {
@@ -20,11 +22,16 @@ public abstract class BlockHandler {
 		final BlockSettings settings = BlockCollection.FLOWER.settings;
 		if( !settings.isEnabled() ) return;
 		
+		// max of 5 attempts per chunk << TODO: make configurable
+		final DecorationGenerator gen = new DecorationGenerator(5);
+        GameRegistry.registerWorldGenerator(gen, 50);
+		
 		final BlockExtraFlower block = new BlockExtraFlower();
 		GameRegistry.registerBlock(block, ExtraItem.class, "flower");
 		
 		for( BlockExtraFlower.BlockType type : BlockExtraFlower.BlockType.values() ) {
 			BiomeCollection.AUTUMN_WOODS.settings.getBiome().get().addFlower(block, type.metadata, type.weight);
+			gen.registerGenerator(type.name(), new WorldGenDecoration(block, type.metadata));
 		}
 		
 		// TODO: register with forestry
