3d620a008716fce2985517d4375c458524a3c721
Ammon Lauritzen <allaryin@gmail.com>
2015-05-09 10:58:10 -0500

    Barest stub for flower generator.

---
+++ src/main/java/extrabiomes/autumn/worldgen/DecorationGenerator.java
@@ -0,0 +1,20 @@
+package extrabiomes.autumn.worldgen;
+
+import java.util.Random;
+
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.IChunkProvider;
+import cpw.mods.fml.common.IWorldGenerator;
+import extrabiomes.lib.ExtraWorldGenerator;
+
+public class DecorationGenerator extends ExtraWorldGenerator implements IWorldGenerator {
+
+	
+	
+	@Override
+	public void generate(Random random, int chunkX, int chunkZ, World world,
+			IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
+		// TODO Auto-generated method stub
+	}
+
+}
