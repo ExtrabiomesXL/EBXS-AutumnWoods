438654eb3e335f609e0d63a9d46b3704a0b81211
Ammon Lauritzen <allaryin@gmail.com>
2015-06-10 11:40:24 -0500

    Bumping version to 0.0.3, API version to 0.1.3.

--- src/main/java/extrabiomes/autumn/Version.java
+++ src/main/java/extrabiomes/autumn/Version.java
@@ -6,7 +6,7 @@ public abstract class Version {
 	public static final String MOD_ID		= "ExtrabiomesAutumnWoods";
 	public static final String MOD_NAME		= "Extrabiomes Autumn Woods";
 	public static final String VERSION		= "@VERSION@";
-	public static final String API_VERSION	= "0.1.2";
+	public static final String API_VERSION	= "0.1.3";
 
 	public static final String LOC_PREFIX   = "extrabiomes.autumn";
 	
